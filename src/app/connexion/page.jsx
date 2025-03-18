954ebbc80ce18213ed4b2f1b6417d7e2b2fed499
LeslieLobry <107678774+LeslieLobry@users.noreply.github.com>
2025-03-18 19:42:48 +0100

    page home

---
+++ src/app/connexion/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+export const metadata = {
+  title: "Connexion",
+  description: "site échangiste. ",
+}
+export default function connexion() {
+  return (
+    <div>
+      Connexion
+    </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,38 @@
 import Image from "next/image";
-// import styles from "./page.module.css";
-import logo from "@/app/Assets/logo.png"
+import "@/app/page.css";
+import logo from "@/app/Assets/logo.png";
+import xperience3 from "@/app/Assets/xperience3.png"
+import { Crimson_Text } from "next/font/google";
+
+const crimsonText = Crimson_Text({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  style: ["normal", "italic"],
+  display: "swap",
+});
 
 export default function Home() {
   return (
-    
-    <div className="contenant">
-      <div className="contenant-header">
-        <Image src={logo} alt="logo de xpérience" width={120} className="contenant-header-logo"></Image>
-        <div className="bouton">
-        <button className="header-inscrition">Inscription</button>
-        <button className="header-connexion">Connexion</button>
+    <div className={crimsonText.className}>
+      <div className="contenant">
+        <div className="contenant-header">
+          <Image
+            src={logo}
+            alt="logo de xpérience"
+            width={200}
+            className="contenant-header-logo"
+          />
+          <div className="bouton-header">
+            <button className="header-inscription">Inscription</button>
+            <button className="header-connexion">Connexion</button>
+          </div>
+        </div>
+        <div className="logo-contenant">
+            <Image src={xperience3}
+            alt="logo de xperience, plus sophistiqué">   
+            </Image>
         </div>
       </div>
     </div>
-  
   );
 }
