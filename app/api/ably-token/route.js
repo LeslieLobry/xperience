e7aa1f9156c2c984065553a346fa0f8101cb021e
LeslieLobry <107678774+LeslieLobry@users.noreply.github.com>
2025-06-16 19:34:19 +0200

    Update route.js

--- app/api/ably-token/route.js
+++ app/api/ably-token/route.js
@@ -1,10 +1,9 @@
-// app/api/ably-token/route.js
 import { NextResponse } from "next/server";
-import Ably from "ably/promises";
+import Ably from "ably";
 
 const apiKey = process.env.NEXT_PUBLIC_ABLY_API_KEY;
 
-export async function GET(req) {
+export async function GET() {
   const client = new Ably.Rest(apiKey);
   const tokenRequest = await client.auth.createTokenRequest({});
   return NextResponse.json(tokenRequest);
