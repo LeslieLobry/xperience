7d8fa94c0c2cd821c78355cf3a77484bf038a8df
LeslieLobry <107678774+LeslieLobry@users.noreply.github.com>
2025-06-16 21:23:06 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,13 @@
-import Image from "next/image";
+// import Image from "next/image";
 import "../app/page.css";
 
-import xperience3 from "../public/images/xperience3.png"
-import masque from "../public/images//masque.png";
+// import xperience3 from "../public/images/xperience3.png"
+// import masque from "../public/images//masque.png";
 import { Crimson_Text } from "next/font/google";
-import Banniere from "../components/Banniere/Banniere"
-import woman from "../public/images/woman.jpg"
-import Button from "../components/Button/Button";
-import bannierehome from "../public/images/bannierehome.png"
+// import Banniere from "../components/Banniere/Banniere"
+// import woman from "../public/images/woman.jpg"
+// import Button from "../components/Button/Button";
+// import bannierehome from "../public/images/bannierehome.png"
 const crimsonText = Crimson_Text({
   subsets: ["latin"],
   weight: ["400", "600", "700"],
@@ -18,19 +18,14 @@ const crimsonText = Crimson_Text({
 export default function Home() {
   return (
     <div className={crimsonText.className}>
-      <div className="contenant">
+      {/* <div className="contenant">
         <div className="logo-contenant">
-            <Image src={xperience3}
-            alt="logo de xperience, plus sophistiqué">   
-            </Image>
+           <Image src="/images/xperience3.png" alt="logo de xperience" width={200} height={100} />
         </div>
         <div className="masque-contenant">
-            <Image src={masque}
-            alt="masque"
-            width={650}
-            ></Image>
-        </div>
-        <div className="text">
+            
+        </div> */}
+        {/* <div className="text">
           <p>Découvrez une communauté où la liberté et le désir se rencontrent. 
           Exprimez-vous sans tabou, seul, en couple ou à plusieurs, et vivez des moments uniques avec des personnes qui partagent vos envies.
           Ici, chaque connexion est une invitation à l’inattendu.
@@ -42,7 +37,7 @@ export default function Home() {
         </div>
         <div className="text-bas"><p>Rejoignez une communauté exclusive où le respect et le plaisir sont maîtres-mots.</p></div>
       <Banniere title='' image={bannierehome}style={{ objectFit: "", width: "100%", height: "auto" }} ></Banniere>
-    </div> 
+    </div>  */}
     </div>
   );
 }
