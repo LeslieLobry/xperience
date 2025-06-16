5a712e61edb97f73aa59d389e2ed3d2387f0b630
LeslieLobry <107678774+LeslieLobry@users.noreply.github.com>
2025-06-16 23:37:35 +0200

    Delete next.config-leslouille.mjs

--- next.config-leslouille.mjs
+++
@@ -1,9 +0,0 @@
-// next.config.mjs
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-    domains: ['xperience-images.s3.eu-west-3.amazonaws.com'],
-  },
-};
-
-export default nextConfig;
