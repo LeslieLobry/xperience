a2171d7310186b979ec1a45e0cb2b1cb58f4ae9e
LeslieLobry <107678774+LeslieLobry@users.noreply.github.com>
2025-07-22 10:54:56 +0200

    lib

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-    domains: ['xperience-images.s3.eu-west-3.amazonaws.com'],
+    domains: ['xperience-images.s3.eu-west-3.amazonaws.com','x-perience-images2.s3.eu-west-3.amazonaws.com'],
+    
   },
 };
 
