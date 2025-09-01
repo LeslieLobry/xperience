65effbeeaaaf836495d8d3129705473d4b398b1a
LeslieLobry <107678774+LeslieLobry@users.noreply.github.com>
2025-09-01 12:08:56 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-    domains: ['xperience-images.s3.eu-west-3.amazonaws.com','x-perience-images2.s3.eu-west-3.amazonaws.com'],
-    
+  images: {
+    domains: [
+      "xperience-images.s3.eu-west-3.amazonaws.com",
+      "x-perience-images2.s3.eu-west-3.amazonaws.com",
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        // Applique les headers CORS à toutes les routes API
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,PATCH,DELETE,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+        ],
+      },
+    ];
   },
 };
 
