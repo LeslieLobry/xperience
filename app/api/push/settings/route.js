aa172fe4a85f351997690b629a48292612e5b175
LeslieLobry <leslielobry@gmail.com>
2025-09-22 14:40:31 +0200

    Update route.js

--- app/api/push/settings/route.js
+++ app/api/push/settings/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import { prisma } from "../../../lib/prisma";
-import { getUserFromToken } from "../../../lib/auth";
+import { prisma } from "../../../../lib/prisma";
+import { getUserFromToken } from "../../../../lib/auth";
 
 /* ---------- CORS ---------- */
 const ALLOWED_ORIGINS = [
