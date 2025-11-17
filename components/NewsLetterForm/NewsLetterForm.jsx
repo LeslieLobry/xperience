0851772bd49bdf2c4142aa7960ba39054772d2a4
LeslieLobry <leslielobry@gmail.com>
2025-11-17 15:37:00 +0100

    Update NewsLetterForm.jsx

--- components/NewsLetterForm/NewsLetterForm.jsx
+++ components/NewsLetterForm/NewsLetterForm.jsx
@@ -1,6 +1,6 @@
 // app/api/newsletter/abonner/route.js
 import { NextResponse } from "next/server";
-import { prisma } from "../../../../lib/prisma";
+import { prisma } from "../../lib/prisma";
 
 export async function POST(req) {
   const { email } = await req.json();
